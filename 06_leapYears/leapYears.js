051c3ad0b53e662ab47913f461885ced1065eecf
j1phan <j1phan@ucsd.edu>
2023-03-26 16:50:33 -0700

    Complete leap year exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (typeof year !== "number") return 'ERROR'
+    let leap = false;
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                leap = true;
+            }
+            else {
+                leap = false;
+            }
+        } else {
+            leap = true;
+        }
+    }
+    return leap;
 };
 
 // Do not edit below this line
