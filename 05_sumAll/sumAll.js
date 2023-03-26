8bb2adb772354742c36ea832185af100186d7086
j1phan <j1phan@ucsd.edu>
2023-03-26 16:44:37 -0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ((num1 < 0) || (num2 < 0)) return 'ERROR';
+    if ((typeof num1 !== "number") || (typeof num2 !== "number")) return 'ERROR';
+    const largerNum = Math.max(num1, num2);
+    const smallerNum = Math.min(num1, num2);
+    let res = 0;
+    for (let i = smallerNum; i <= largerNum; i++) {
+        res += i;
+    }
+    return res
 
 };
 
