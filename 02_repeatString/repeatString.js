4a41ff13f0be84782a263890f3c26cbaf65d108e
j1phan <j1phan@ucsd.edu>
2023-03-26 19:24:54 -0700

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, numRepeated) {
+    let result = '';
+    if (numRepeated < 0) return 'ERROR';
+    for (let i = 0; i < numRepeated; i++) {
+        result += inputString;
+    }
+    return result;
 };
 
 // Do not edit below this line
